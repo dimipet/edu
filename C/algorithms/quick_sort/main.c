c4113647474191d898e27ac93780e0ce9a9dd7c6
dimipet <dimi.petridis@gmail.com>
2021-04-16 14:03:32 +0300

    refactors + sanity changes

--- C/algorithms/quick_sort/main.c
+++ C/algorithms/quick_sort/main.c
@@ -6,69 +6,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//int A[10]; //έστω πίνακας Α
+const int ARR_LENGTH = 100;
 
-void print_array(int A[], int n){
+void print_array(int arr[], int n){
   int i;
   for (i = 0; i < n; i++) {
-    printf("%d\n", A[i]);
+    printf("%d\n", arr[i]);
   }
 }
 
 
 //συνάρτηση quick_sort
-void quick_sort(int A[], int a, int b) {
-
+void quick_sort(int my_array[], int a, int b) {
     //τοπικές μεταβλητές
 
-    //όσο και το μέγεθος του Α
-    int leftarr[10]; //νέος πίνακας (δεξιός)
-    int rtarr[10]; //νέος πίνακας (αριστερός)
+    //όσο και το μέγεθος του my_array
+    int leftarr[ARR_LENGTH]; //νέος πίνακας (δεξιός)
+    int rtarr[ARR_LENGTH]; //νέος πίνακας (αριστερός)
     int ltidx = 0; //δείκτης δεξιού πίνακα leftarr[]
     int rtidx = 0; //δείκτης αριστερού πίνακα rtarr[]
     int k = a;
     int l = 0;
 
     //δημιουργία και αρχικοποίηση pivot (βλ.Θεωρία)
-    int pivot = A[a];
+    int pivot = my_array[a];
 
     if (a == b) return; //συνθήκη τερματισμού αναδρομής
 
     while (k < b) {
         ++k;
-        //εάν στοιχείο Α[k] μικρότερο A[a]
-        if (A[k] < A[a]) {
-            //πρόσθεσε στον αριστερό πίνακα leftarr το A[k]
-            leftarr[ltidx] = A[k];
+        //εάν στοιχείο my_array[k] μικρότερο my_array[a]
+        if (my_array[k] < my_array[a]) {
+            //πρόσθεσε στον αριστερό πίνακα leftarr το my_array[k]
+            leftarr[ltidx] = my_array[k];
             //αύξηση δείκτη πίνακα leftarr
             ltidx++;
         }   //αλλιώς αν μεγαλύτερο ή ίσο
         else {
-            //πρόσθεσε στον δεξί πίνακα rtarr το A[k]
-            rtarr[rtidx] = A[k];
+            //πρόσθεσε στον δεξί πίνακα rtarr το my_array[k]
+            rtarr[rtidx] = my_array[k];
             //αύξηση δείκτη πίνακα leftarr
             rtidx++;
         }//τέλος if else
     }//τέλος while
 
     k = a;
-    for (l = 0; l < ltidx; ++l)A[k++] = leftarr[l]; //τέλος for
-    A[k++] = pivot;
-    for (l = 0; l < rtidx; ++l)A[k++] = rtarr[l]; //τέλος for
+    for (l = 0; l < ltidx; ++l) my_array[k++] = leftarr[l]; //τέλος for
+    my_array[k++] = pivot;
+    for (l = 0; l < rtidx; ++l) my_array[k++] = rtarr[l]; //τέλος for
 
     //αναδρομή για επιμέρους πίνακες που δημιουργήθηκαν
-    if (ltidx > 0)quick_sort(A, a, a + ltidx - 1);
-    if (rtidx > 0)quick_sort(A, b - rtidx + 1, b);
+    if (ltidx > 0) quick_sort(my_array, a, a + ltidx - 1);
+    if (rtidx > 0) quick_sort(my_array, b - rtidx + 1, b);
 
 }//τέλος quick_sort συνάρτησης
 
 int main(int argc, char** argv) {
-  int A[10];
+
+  int arr[ARR_LENGTH];
   int i;
-  for (i = 0; i < 10; i++) {
-    A[i] = rand();
+  for (i = 0; i < ARR_LENGTH; i++) {
+    arr[i] = rand();
   }
-  quick_sort(A, 0, 9);
-  print_array(A, 10);
+  quick_sort(arr, 0, ARR_LENGTH-1);
+  print_array(arr, ARR_LENGTH);
   return (EXIT_SUCCESS);
 }
